34840801cf928854483f5e2b304696f63b473dd5
Lauren Hallau <lhallau@gmail.com>
2020-02-28 19:14:40 -0600

    intern and engineer questions complete

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output", "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 const teamMembers = [];
@@ -82,13 +83,137 @@ function createTeam() {
         switch (answers.teamMemberChoice) {
             case "Engineer":
                 console.log("engineer");
+                createEngineer();
                 break;
             case "Intern" :
                 console.log("intern");
+                createIntern();
                 break;
+            case "End" :
+                console.log("ending process");
+                buildTeam();    
             default : 
                 console.log("build your team");
         }
     })
 }
+function createEngineer() {
+    inquirer
+    .prompt([
+        {
+            type : "input",
+            name : "engineerName",
+            message: "Please enter the Engineer's name",
+            validate: answer => {
+                if(answer !== "") {
+                    return true;
+                }
+                return "Please enter complete name";
+            }
+        },
+        {
+            type : "input",
+            name : "engineerID",
+            message: "Please enter the Enginer's ID",
+            validate: answer => {
+                var pass = answer.match( /^[1-9]\d*$/); 
+                if(pass) {
+                    return true
+                }
+                return "Please enter a number greater than zero";
+            }
+        },
+        {
+            type : "input",
+            name : "engineerEmail",
+            message: "Please enter the Engineer's email",
+            validate: answer => {
+                var pass = answer.match( /\S+@\S+\.\S+/); 
+                if(pass) {
+                    return true
+                }
+                return "Please enter a valid email";
+            }
+        },
+        {
+            type : "input",
+            name : "engineerGitHub",
+            message: "Please enter the Engineer's GitHub",
+            validate: answer => {
+                if(answer !== "") {
+                    return true;
+                }
+                return "Please enter complete name";
+            }
+        },
+    ]).then(answers => {
+        console.log(answers);
+        const engineer = new Engineer (answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerOffice);
+        console.log(engineer);
+        teamMembers.push(engineer);
+        createTeam();
+    })
+}
+function createIntern() {
+    inquirer
+    .prompt([
+        {
+            type : "input",
+            name : "internName",
+            message: "Please enter the Intern's name",
+            validate: answer => {
+                if(answer !== "") {
+                    return true;
+                }
+                return "Please enter complete name";
+            }
+        },
+        {
+            type : "input",
+            name : "internID",
+            message: "Please enter the Intern's ID",
+            validate: answer => {
+                var pass = answer.match( /^[1-9]\d*$/); 
+                if(pass) {
+                    return true
+                }
+                return "Please enter a number greater than zero";
+            }
+        },
+        {
+            type : "input",
+            name : "internEmail",
+            message: "Please enter the Intern's email",
+            validate: answer => {
+                var pass = answer.match( /\S+@\S+\.\S+/); 
+                if(pass) {
+                    return true
+                }
+                return "Please enter a valid email";
+            }
+        },
+        {
+            type : "input",
+            name : "internSchool",
+            message: "Please enter the Intern's school",
+            validate: answer => {
+                if(answer !== "") {
+                    return true;
+                }
+                return "Please enter complete name";
+            }
+        },
+    ]).then(answers => {
+        console.log(answers);
+        const intern = new Intern (answers.internName, answers.internID, answers.internEmail, answers.internOffice);
+        console.log(intern);
+        teamMembers.push(intern);
+        createTeam();
+    })
+}
 createManager();
+
+function buildTeam() {
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+    console.log(buildTeam());
+  }
